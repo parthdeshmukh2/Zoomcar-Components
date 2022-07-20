370702def7d238402bc5098b943c48339595820b
parthdeshmukh2 <parthdeshmukh99@gmail.com>
2022-07-20 12:57:41 +0530

    hero banner done

---
+++ zoomcar/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Zms from './Pages/Zms';
+
+function App() {
+  return (
+    <div className="App">
+    <Zms/>
+    </div>
+  );
+}
+
+export default App;
---
+++ zoomcar/src/Pages/Zms.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroBanner from '../Components/HeroBanner'
+
+const Zms = () => {
+  return (
+    <div>
+        <HeroBanner/>
+      
+    </div>
+  )
+}
+
+export default Zms

f7450aad49a6d2a3ea710cf39f0988a4272c95fa
parthdeshmukh2 <parthdeshmukh99@gmail.com>
2022-07-20 13:52:08 +0530

    fotter done

--- zoomcar/src/Pages/Zms.jsx
+++ zoomcar/src/Pages/Zms.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeroBanner from '../Components/HeroBanner'
+import ZmsFooter from '../Components/ZmsFooter'
 
 const Zms = () => {
   return (
     <div>
         <HeroBanner/>
+        <ZmsFooter/>
+
+
       
     </div>
   )
